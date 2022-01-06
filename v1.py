af85e2d96db46d65ba3d7f52e4c231920c5b7f28
jonec <jonec9676@gmail.com>
2022-01-06 22:22:13 +0800

    Finish basic V1

--- v1.py
+++ v1.py
@@ -2,27 +2,39 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 import numpy as np
 
-## 參數設定
 PHASE_COUNT = 5 # 5 階段
 cost = [0]*PHASE_COUNT
 profit = [0]*PHASE_COUNT
-ETH = 3863
-mint_price = [0.044, 0.08, 0.08, 0.08, 0.08] # eth
-gift = [44*444, 1444*34, 2444*24+4*ETH, 3444*14+21.6*ETH, 4444*4+49.6*ETH+54496.90]
-threshhold = [444, 1444, 2444, 3444, 4444]
-lucky_prize = 44 # U
-
 tmp_cost = []
 tmp_profit = []
 
+## 參數設定
+ETH = 3863
+target_profit = 200*ETH
+
+## 成本參數
+gas_fee = 40
+phase_gas_fee = [0, 0, 4*gas_fee, 8*gas_fee, 8*gas_fee] # 前兩個階段沒有送 eth，後三個階段才有
+lucky_prize = 44 # U
+free_air_drop = 270 # 空投給虎友
+gift = [44*444, 34*1444, 24*2444+4*ETH, 14*3444+21.6*ETH, 4*4444+49.6*ETH+54496.90]
+threshhold = [444, 1444, 2444, 3444, 4444]
+
+## 獲利參數
+mint_price = [0.044, 0.08, 0.08, 0.08, 0.08] # eth
+
+if free_air_drop >= threshhold[0]:
+    print("空投 & 門檻的比例不對！")
+
 phase = 0
 for i in range(1, 4445):
-    if i <= threshhold[phase]:
+    if i <= threshhold[phase] and i > free_air_drop:
         profit[phase] += mint_price[phase]*ETH
         if (len(str(i))>2 and str(i)[-3:] == "444") or (i%44==0):
             cost[phase] += lucky_prize
         if i == threshhold[phase]:
             cost[phase] += gift[phase]
+            cost[phase] += phase_gas_fee[phase]
             phase += 1
     tmp_cost.append(sum(cost))
     tmp_profit.append(sum(profit))
@@ -31,8 +43,13 @@ x = np.arange(1, 4445, 1)
 
 # 淨值計算
 net_value = []
+show_target_idx = False
 for i in range(4444):
-    net_value.append(tmp_profit[i] - tmp_cost[i])
+    tmp_net = tmp_profit[i] - tmp_cost[i]
+    if not show_target_idx and tmp_net >= target_profit:
+        print("賣 {0} 包，即可達到目標獲利 : {1} U".format(i+1, target_profit))
+        show_target_idx = True
+    net_value.append(tmp_net)
 
 
 plt.rcParams['font.family'] = ['Heiti TC']
@@ -49,19 +66,19 @@ for x,y in zip(x, net_value):
     ## 印出門檻值之前一個點 (ex: 443, 1443 ...)
     if idx == threshhold[tmp_phase]-1:
         label = "{:.2f}".format(y)
-        plt.annotate(label, # this is the text
-                    (x,y), # this is the point to label
-                    textcoords="offset points", # how to position the text
-                    xytext=(-3,3), # distance from text to points (x,y)
+        plt.annotate(label, 
+                    (x,y), 
+                    textcoords="offset points", 
+                    xytext=(-3,3),
                     ha='left')
     ## 印出門檻值 (ex: 444, 1444 ...)
     if idx == threshhold[tmp_phase]:
         tmp_phase += 1
         label = "{:.2f}".format(y)
-        plt.annotate(label, # this is the text
-                    (x,y), # this is the point to label
-                    textcoords="offset points", # how to position the text
-                    xytext=(3,-3), # distance from text to points (x,y)
+        plt.annotate(label, 
+                    (x,y), 
+                    textcoords="offset points", 
+                    xytext=(3,-3), 
                     ha='left')
     idx += 1
 
