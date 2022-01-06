99b12beac0dcd42665e553a476cdb1b4959d4098
jonec <jonec9676@gmail.com>
2022-01-06 17:52:14 +0800

    Add more comments

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.pyplot import figure
-from matplotlib import font_manager
-plt.rcParams['font.family'] = ['Heiti TC']
-fig = plt.gcf()
-fig.set_size_inches(18.5, 6.5)
-fig.savefig('test2png.png', dpi=100)
+import numpy as np
 
-
-cost = [0]*5
-phase = [False]*5
-profit = [0]*5
+## 參數設定
+PHASE_COUNT = 5 # 5 階段
+cost = [0]*PHASE_COUNT
+profit = [0]*PHASE_COUNT
 ETH = 3863
 mint_price = [0.044, 0.08, 0.08, 0.08, 0.08] # eth
 gift = [44*444, 1444*34, 2444*24+4*ETH, 3444*14+21.6*ETH, 4444*4+49.6*ETH+54496.90]
-
-
 threshhold = [444, 1444, 2444, 3444, 4444]
 lucky_prize = 44 # U
 
@@ -36,18 +29,24 @@ for i in range(1, 4445):
 
 x = np.arange(1, 4445, 1)
 
-pure = []
+# 淨值計算
+net_value = []
 for i in range(4444):
-    pure.append(tmp_profit[i] - tmp_cost[i])
+    net_value.append(tmp_profit[i] - tmp_cost[i])
 
-plt.plot(x, pure, label="淨利 (獲利 - 成本)")
+
+plt.rcParams['font.family'] = ['Heiti TC']
+fig = plt.gcf()
+fig.set_size_inches(18.5, 6.5)
+
+plt.plot(x, net_value, label="淨利 (獲利 - 成本)")
 plt.xticks(np.arange(min(x), max(x)+1, 200.0))
-plt.yticks(np.arange(min(pure), max(pure)+1, (max(pure)-min(pure))/20))
-
+plt.yticks(np.arange(min(net_value), max(net_value)+1, (max(net_value)-min(net_value))/20))
 
 idx = 1
-tmp_phase = 0
-for x,y in zip(x, pure):
+tmp_phase = 0 # 0~5
+for x,y in zip(x, net_value):
+    ## 印出門檻值之前一個點 (ex: 443, 1443 ...)
     if idx == threshhold[tmp_phase]-1:
         label = "{:.2f}".format(y)
         plt.annotate(label, # this is the text
@@ -55,7 +54,7 @@ for x,y in zip(x, pure):
                     textcoords="offset points", # how to position the text
                     xytext=(-3,3), # distance from text to points (x,y)
                     ha='left')
-
+    ## 印出門檻值 (ex: 444, 1444 ...)
     if idx == threshhold[tmp_phase]:
         tmp_phase += 1
         label = "{:.2f}".format(y)
@@ -66,9 +65,8 @@ for x,y in zip(x, pure):
                     ha='left')
     idx += 1
 
-# plt.xlabel('Mint 捲軸數目', fontsize=20)
-# plt.ylabel('虎群獲利', fontsize=20)
-# plt.plot(x, tmp_cost)
-# plt.plot(x, tmp_profit)
+plt.xlabel('Mint 捲軸數目', fontsize=20)
+plt.ylabel('虎群獲利', fontsize=20)
+plt.savefig("v1.png")
 plt.legend()
 plt.show()
